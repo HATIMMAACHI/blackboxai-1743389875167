9f985292ab048e469a79a98b1374d341c971cee9
Hatim <125673553+HATIMMAACHI@users.noreply.github.com>
2025-03-31 04:59:20 +0200

    Add TECHWEB/marwane/marwane/src/main/java/com/conference/model/User.java 1743389959503

---
+++ TECHWEB/marwane/marwane/src/main/java/com/conference/model/User.java
@@ -0,0 +1,109 @@
+package com.conference.model;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String role; // PRESIDENT, COMMITTEE_MEMBER, AUTHOR, REVIEWER
+    private String affiliation;
+    private String academicBackground;
+
+    // Constructors
+    public User() {}
+
+    public User(String username, String email, String firstName, String lastName, String role) {
+        this.username = username;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.role = role;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getAffiliation() {
+        return affiliation;
+    }
+
+    public void setAffiliation(String affiliation) {
+        this.affiliation = affiliation;
+    }
+
+    public String getAcademicBackground() {
+        return academicBackground;
+    }
+
+    public void setAcademicBackground(String academicBackground) {
+        this.academicBackground = academicBackground;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
